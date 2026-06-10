1618fe8d84c24b95e8b4d7bad69a3438d584ec0c
badapyobum <gkstnghkdlxld@gmail.com>
2026-06-10 14:55:03 +0900

    test2

--- src/main/java/com/aiclass03team07/bookapp/service/bookdetail/BookDetailService.java
+++ src/main/java/com/aiclass03team07/bookapp/service/bookdetail/BookDetailService.java
@@ -1,8 +1,11 @@
 package com.aiclass03team07.bookapp.service.bookdetail;
 
 import com.aiclass03team07.bookapp.dto.bookcreate.BookCreateDTO;
+import com.aiclass03team07.bookapp.dto.bookdetail.DetailDTO;
 import com.aiclass03team07.bookapp.entity.BookEntity;
+import com.aiclass03team07.bookapp.entity.GenerateImageEntity;
 import com.aiclass03team07.bookapp.repository.BookRepository;
+import com.aiclass03team07.bookapp.repository.GenerateImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,36 @@ import org.springframework.stereotype.Service;
 public class BookDetailService {
 
     private final BookRepository bookRepository;
+    private final GenerateImageRepository generateImageRepository;
 
+    //id 정보조회
+    public DetailDTO getBookDetail(Long id){
+
+        BookEntity entity = bookRepository.findById(id).orElse(null);
+        GenerateImageEntity generateImage = entity.getGenerateImageEntity();
+        Long generateImageId = (generateImage != null) ? generateImage.getId() : null;
+        DetailDTO dto = new DetailDTO();
+        dto.setLikes(entity.getLikes());
+        dto.setContent(entity.getContent());
+        dto.setGenre(entity.getGenre());
+        dto.setAuthor(entity.getAuthor());
+        dto.setPublisher(entity.getPublisher());
+        dto.setTitle(entity.getTitle());
+        dto.setCreatedAt(entity.getCreatedAt());
+        dto.setSeriesInfo(entity.getSeriesInfo());
+        dto.setUpdatedAt(entity.getUpdatedAt());
+        dto.setPublicationDt(entity.getPublishedDt());
+        if(generateImageId != null){
+            String img_url = generateImageRepository.findById(generateImageId)
+                    .map(GenerateImageEntity::getCoverImageUrl)
+                    .orElse(null);
+            dto.setCoverImageUrl(img_url);
+        }
+        else
+            dto.setCoverImageUrl(null);
+
+        return dto;
+    }
     //ㅁㄴ
     // 좋아요 +1
     public BookCreateDTO updateLikes(Long id) {
