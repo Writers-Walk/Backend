f5a9c096ae558f7510c1a0cb3efc8d4f31dc918c
badapyobum <gkstnghkdlxld@gmail.com>
2026-06-11 14:01:26 +0900

    mainbanner

---
+++ src/main/java/com/aiclass03team07/bookapp/dto/mainpage/MainBannerDTO.java
@@ -0,0 +1,11 @@
+package com.aiclass03team07.bookapp.dto.mainpage;
+
+import lombok.Data;
+
+@Data
+public class MainBannerDTO {
+    private Long id;
+    private String title;
+    private String coverImageUrl;
+
+}
