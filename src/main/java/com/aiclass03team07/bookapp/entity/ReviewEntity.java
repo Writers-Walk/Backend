c6054381fb973b88e2d441d0bd39132be9df122d
jun9771 <jun97718@gmail.com>
2026-06-11 10:16:30 +0900

    review-api-specification

---
+++ src/main/java/com/aiclass03team07/bookapp/entity/ReviewEntity.java
@@ -0,0 +1,32 @@
+package com.aiclass03team07.bookapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class ReviewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rating")
+    private Long rating;
+
+    @CreationTimestamp
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    //여기 책 과 1대1 연결
+
+    //여기 user랑 1대n 연결
+
+}
