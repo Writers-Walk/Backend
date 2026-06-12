570263e2f5f56bd514175247e7cfc3be107d5c60
jun9771 <jun97718@gmail.com>
2026-06-12 13:09:14 +0900

    ff345t

--- src/main/java/com/aiclass03team07/bookapp/BookappApplication.java
+++ src/main/java/com/aiclass03team07/bookapp/BookappApplication.java
@@ -14,36 +14,6 @@ public class BookappApplication {
 		SpringApplication.run(BookappApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner init(DetailRepository detailRepository) {
-//		return args -> {
-//			BookDetail b1 = new BookDetail();
-//			b1.setTitle("자바의 정석");
-//			b1.setAuthor("남궁성");
-//			detailRepository.save(b1);
-//
-//			BookDetail b2 = new BookDetail();
-//			b2.setTitle("Spring 입문");
-//			b2.setAuthor("임한울");
-//			detailRepository.save(b2);
-//
-//			BookDetail b3 = new BookDetail();
-//			b3.setTitle("React 시작");
-//			b3.setAuthor("홍길동");
-//			detailRepository.save(b3);
-//
-//			BookDetail b4 = new BookDetail();
-//			b4.setTitle("자바의 기초");
-//			b4.setAuthor("임한울");
-//			detailRepository.save(b4);
-//
-//			BookDetail b5 = new BookDetail();
-//			b5.setTitle("node.js 실전");
-//			b5.setAuthor("김에이블");
-//			detailRepository.save(b5);
-//		};
-//	}
-
 	//GenerateImage Test용
 	@Bean
 	CommandLineRunner initImageTest(BookRepository bookRepository) {
